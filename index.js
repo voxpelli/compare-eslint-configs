3d051d7cf75f1e6fde6e413e25215570b75d6ca1
Pelle Wessman <pelle@kodfabrik.se>
2022-09-21 14:30:36 +0200

    More WIP

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-console */
-
 import { execa } from 'execa';
-import chalk from 'chalk';
 
 import { compareConfigs } from './lib/compare.js';
+import { printComparationResult } from './lib/print-result.js';
 import { zipObject } from './lib/utils.js';
 
 /** @type {['airbnb', 'standard']} */
@@ -11,6 +9,10 @@ const configNames = [
   'airbnb',
   'standard',
 ];
+
+// TODO: First check that both configs exist? And/or generate the configs on the fly?
+// TODO: Make target file configurable?
+
 const executionOutput = (await Promise.all([
   execa('npx eslint --no-eslintrc -c source.eslintrc.json --print-config index.js', { cwd: new URL('./', import.meta.url) }),
   execa('npx eslint --no-eslintrc -c .eslintrc --print-config index.js', { cwd: new URL('./', import.meta.url) })
@@ -20,37 +22,6 @@ const executionOutput = (await Promise.all([
 const parsedOutput = executionOutput.map(output => JSON.parse(output.stdout));
 
 const configs = zipObject(configNames, parsedOutput);
+const differences = compareConfigs(configs);
 
-const {
-  mixedConfigs,
-  mixedSeverity,
-  onlyActiveIn
-} = compareConfigs(configs);
-
-for (const configName of Object.keys(onlyActiveIn).sort()) {
-  console.log(`Only active in ${configName}:`);
-  for (const ruleName of onlyActiveIn[configName]?.sort() || []) {
-    console.log(chalk[configName === 'standard' ? 'green' : 'red'](`  ${ruleName}`));
-  }
-}
-
-console.log('Mixed severities:');
-for (const [ruleName, severities] of Object.entries(mixedSeverity)) {
-  const formattedSeverities = Object.entries(severities)
-    .map(
-      ([level, configNames]) =>
-        level + ': ' + configNames.join(', ')
-    );
-  console.log(`  ${ruleName}: ${formattedSeverities.join(', ')}`);
-}
-
-console.log('Mixed configs where otherwise okay:');
-for (const ruleName of Object.keys(mixedConfigs).sort()) {
-  console.log(`  ${ruleName}:`);
-  const mixedConfig = mixedConfigs[ruleName];
-  if (mixedConfig) {
-    for (const configName of Object.keys(mixedConfig).sort()) {
-      console.log(chalk[configName === 'standard' ? 'green' : 'red'](`    ${JSON.stringify(mixedConfig[configName])}`));
-    }
-  }
-}
+printComparationResult(differences);
---
+++ test/compare.spec.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+
+import { compareConfigs } from '../lib/compare.js';
+
+chai.should();
+
+describe('compare', () => {
+  it('should work', async () => {
+    // TODO: Make it sensible
+    const differences = await compareConfigs({});
+    differences.should.equal({});
+  });
+});
