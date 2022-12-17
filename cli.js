a290255a0f071ef5fbaaf24b6ed6829a916c5ee4
Pelle Wessman <pelle@kodfabrik.se>
2022-12-17 20:10:53 +0100

    Add summary view

--- cli.js
+++ cli.js
@@ -10,20 +10,29 @@ import { ESLint } from 'eslint';
 import meow from 'meow';
 import { messageWithCauses, stackWithCauses } from 'pony-cause';
 
-import { compareConfigs } from './lib/compare.js';
+import { compareConfigs, summarizeConfigs } from './lib/compare.js';
 import { printComparationResult } from './lib/print-result.js';
 import { zipObject } from './lib/utils.js';
+import { printConfigSummary } from './lib/print-summary.js';
 
 const cli = meow(`
   Usage
     $ compare-eslint <eslint config files, separated by spaces>
 
-  Compares the provided eslint config files.
+  Compares or summarizes the provided eslint config file(s).
 
-  Options
-    -m, --markdown     Format the result as Markdown instead. Useful for copy and pasting into eg. GitHub.
-    -r, --group-rules  Prints the rules that only exists in some configs grouped by rule rather than config.
-    -t, --target-file  The file context which the eslint config should be compared in. Defaults to index.js
+  Presentation options
+    --group-rules  -r  Prints the rules that only exists in some configs grouped by rule rather than config.
+    --markdown     -m  Format the result as Markdown instead. Useful for copy and pasting into eg. GitHub.
+    --no-links         Skips adding hyperlinks to rule documentation
+    --table            Prints the output in a table format rather than list format
+    --verbose-configs  Includes full config data in the output, even the complex one
+
+  Comparison options
+    --summary      -s  Prints a summary of the specified configs instead
+    --target-file  -t  The file context which the eslint config should be compared in. Defaults to index.js
+
+  Additional options
     --help             Print this help and exits.
     --version          Prints current version and exits.
 
@@ -32,44 +41,49 @@ const cli = meow(`
 `, {
   importMeta: import.meta,
   flags: {
-    groupRules: {
-      alias: 'r',
-      type: 'boolean',
-      'default': false
-    },
-    markdown: {
-      alias: 'm',
-      type: 'boolean',
-      'default': false
-    },
-    targetFile: {
-      alias: 't',
-      type: 'string',
-      'default': 'index.js'
-    },
+    groupRules: { alias: 'r', type: 'boolean', 'default': false },
+    links: { type: 'boolean', 'default': true },
+    markdown: { alias: 'm', type: 'boolean', 'default': false },
+    summary: { alias: 's', type: 'boolean', 'default': false },
+    table: { type: 'boolean', 'default': false },
+    targetFile: { alias: 't', type: 'string', 'default': 'index.js' },
+    verboseConfigs: { type: 'boolean', 'default': false },
   }
 });
 
 const {
   groupRules: groupByRule,
+  links,
   markdown,
+  summary,
+  table,
   targetFile,
+  verboseConfigs,
 } = cli.flags;
 
 const configFiles = [...cli.input];
 
-const targetAbsolute = path.resolve(cwd(), targetFile);
-
 if (configFiles.length === 0) {
-  cli.showHelp();
-  process.exit(1);
-} else if (configFiles.length === 1) {
+  if (!summary) {
+    cli.showHelp();
+    process.exit(1);
+  }
+  configFiles.unshift('.eslintrc');
+} else if (configFiles.length === 1 && !summary) {
+  if (configFiles[0] === '.eslintrc') {
+    console.error(chalk.bgRed('Can\'t compare:') + ' There is nothing to compare .eslintrc to, add another config');
+    process.exit(1);
+  }
+
   configFiles.unshift('.eslintrc');
 }
 
-const targetStat = await stat(targetAbsolute);
-if (!targetStat.isFile) {
-  console.error(chalk.bgRed('Invalid target:') + ` Can't find target file "${targetAbsolute}"\n`);
+const targetAbsolute = path.resolve(cwd(), targetFile);
+const targetStat = await stat(targetAbsolute).catch(() => {});
+
+if (!targetStat || !targetStat.isFile) {
+  // TODO: Add a fallback that automatically finds a JS file in the current folder
+  console.error(chalk.bgRed('Invalid target:') + ` Can't find "${targetAbsolute}", set a target with --target-file`);
   process.exit(1);
 }
 
@@ -95,12 +109,28 @@ try {
 
   const configs = zipObject(configFiles, await Promise.all(executions));
 
-  const differences = compareConfigs(configs);
+  if (summary) {
+    for (const [configName, config] of Object.entries(configs)) {
+      const summarizedRules = summarizeConfigs({ [configName]: config });
 
-  printComparationResult(differences, configFiles, {
-    groupByRule,
-    markdown,
-  });
+      printConfigSummary(configName, summarizedRules, {
+        markdown,
+        skipLinks: !links,
+        table,
+        verboseConfigs,
+      });
+    }
+  } else {
+    const differences = compareConfigs(configs);
+
+    printComparationResult(differences, configFiles, {
+      groupByRule,
+      markdown,
+      skipLinks: !links,
+      table,
+      verboseConfigs,
+    });
+  }
 } catch (err) {
   console.error(
     chalk.bgRed('Unexpected error:') +
