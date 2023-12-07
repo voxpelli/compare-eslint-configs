ca6cba41a1b4be615dd96ba62da876e70ebfbef1
Pelle Wessman <pelle@kodfabrik.se>
2023-12-07 15:02:38 +0100

    Add JSON-based diff output

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ import { ESLint } from 'eslint';
 import meow from 'meow';
 import { messageWithCauses, stackWithCauses } from 'pony-cause';
 
-import { compareConfigs, summarizeConfigs } from './lib/compare.js';
+import { compareConfigs, diffConfigs, summarizeConfigs } from './lib/compare.js';
 import { printComparationResult } from './lib/print-result.js';
 import { zipObject } from './lib/utils.js';
 import { printConfigSummary } from './lib/print-summary.js';
@@ -23,6 +23,7 @@ const cli = meow(`
 
   Presentation options
     --group-rules  -r  Prints the rules that only exists in some configs grouped by rule rather than config.
+    --json         -j  Output the results as JSON instead.
     --markdown     -m  Format the result as Markdown instead. Useful for copy and pasting into eg. GitHub.
     --no-links         Skips adding hyperlinks to rule documentation
     --table            Prints the output in a table format rather than list format
@@ -30,6 +31,7 @@ const cli = meow(`
 
   Comparison options
     --summary      -s  Prints a summary of the specified configs instead
+    --diff         -d  Prints what's changed between the second and the first file
     --target-file  -t  The file context which the eslint config should be compared in. Defaults to index.js
 
   Additional options
@@ -41,7 +43,9 @@ const cli = meow(`
 `, {
   importMeta: import.meta,
   flags: {
+    diff: { alias: 'd', type: 'boolean', 'default': false },
     groupRules: { alias: 'r', type: 'boolean', 'default': false },
+    json: { alias: 'j', type: 'boolean', 'default': false },
     links: { type: 'boolean', 'default': true },
     markdown: { alias: 'm', type: 'boolean', 'default': false },
     summary: { alias: 's', type: 'boolean', 'default': false },
@@ -52,7 +56,9 @@ const cli = meow(`
 });
 
 const {
+  diff,
   groupRules: groupByRule,
+  json: jsonOutput,
   links,
   markdown,
   summary,
@@ -60,6 +66,19 @@ const {
   targetFile,
   verboseConfigs,
 } = cli.flags;
+
+if (markdown && jsonOutput) {
+  console.error(chalk.bgRed('Conflicting options:') + ' Can\'t chose both JSON and Markdown at once');
+  process.exit(1);
+}
+if (diff && summary) {
+  console.error(chalk.bgRed('Conflicting options:') + ' Can\'t chose both diff and summary at once');
+  process.exit(1);
+}
+if (jsonOutput && !diff) {
+  console.error(chalk.bgRed('Unsupported output:') + ' JSON output is currently only supported for diffs');
+  process.exit(1);
+}
 
 const configFiles = [...cli.input];
 
@@ -121,6 +140,48 @@ try {
         table,
         verboseConfigs,
       });
+    }
+  } else if (diff) {
+    if (!jsonOutput) {
+      console.error(chalk.bgRed('Unsupported output:') + ' JSON output is currently the only supported output for diffs');
+      process.exit(1);
+    }
+    if (configFiles.length > 2) {
+      console.error(chalk.bgRed('Too many configs:') + ' Diff calculation only works when given two configs');
+      process.exit(1);
+    }
+
+    const targetConfigName = configFiles[0] || '';
+    const sourceConfigName = configFiles[1] || '';
+
+    const targetConfig = configs[targetConfigName];
+    const sourceConfig = configs[sourceConfigName];
+
+    if (!targetConfig || !sourceConfig) {
+      console.error(chalk.bgRed('Too few configs:') + ' Diff calculation only works when given two configs');
+      process.exit(1);
+    }
+
+    const diffResult = diffConfigs(
+      { configName: targetConfigName, config: targetConfig },
+      { configName: sourceConfigName, config: sourceConfig }
+    );
+
+    if (diffResult) {
+      const {
+        added,
+        changedConfig,
+        changedSeverity,
+        removed,
+      } = diffResult;
+
+      console.log(JSON.stringify({
+        added,
+        changedConfig,
+        changedSeverity,
+        removed,
+      }, undefined, 2));
+      process.exit(2);
     }
   } else {
     const differences = compareConfigs(configs);
